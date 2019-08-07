5e4f1b53bcbb1a5f58d7fb67c23dca8b03d4742a
Marvin Preuss <marvin@xsteadfastx.org>
2019-08-07 09:09:16 +0200

    build different kind of firmwares

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: all clean init test formatting rshell copy build-builder build-henri-firmware build-plain-firmware flash
+.PHONY: all clean init test formatting rshell copy build-builder build-henri-firmware build-deps-firmware build-plain-firmware flash
 
 init:
 	poetry install
@@ -22,10 +22,15 @@ build-builder:
 	docker build -f Dockerfile.build -t henri-builder .
 
 build-henri-firmware:
-	docker run --rm -ti -v $(PWD):/origin:ro -v $(PWD)/build:/build -e "HENRI=True" henri-builder sh /origin/build.sh
+	docker run --rm -ti -v $(PWD):/origin:ro -v $(PWD)/build:/build -e "HENRI=True" -e "DEPS=True" henri-builder sh /origin/build.sh
+
+build-deps-firmware:
+	docker run --rm -ti -v $(PWD):/origin:ro -v $(PWD)/build:/build -e "HENRI=False" -e "DEPS=True" henri-builder sh /origin/build.sh
 
 build-plain-firmware:
-	docker run --rm -ti -v $(PWD):/origin:ro -v $(PWD)/build:/build -e "HENRI=False" henri-builder sh /origin/build.sh
+	docker run --rm -ti -v $(PWD):/origin:ro -v $(PWD)/build:/build -e "HENRI=False" -e "DEPS=False" henri-builder sh /origin/build.sh
+
+all: build-henri-firmware build-deps-firmware build-plain-firmware
 
 flash:
 	poetry run esptool.py --port /dev/ttyUSB0 erase_flash
--- build.sh
+++ build.sh
@@ -9,18 +9,20 @@ sudo chown -R builder /henri
 sudo chown -R builder /build
 
 # freeze modules
-## pkg_resources
-ln -s /henri/src/lib/pkg_resources.py /henri/submodules/pycopy/ports/esp32/modules/pkg_resources.py
-## uasyncio
-mkdir /henri/submodules/pycopy/ports/esp32/modules/uasyncio
-ln -s /henri/src/lib/uasyncio/__init__.py /henri/submodules/pycopy/ports/esp32/modules/uasyncio/__init__.py
-ln -s /henri/src/lib/uasyncio/core.py /henri/submodules/pycopy/ports/esp32/modules/uasyncio/core.py
-## picoweb
-mkdir /henri/submodules/pycopy/ports/esp32/modules/picoweb
-ln -s /henri/src/lib/picoweb/__init__.py /henri/submodules/pycopy/ports/esp32/modules/picoweb/__init__.py
-ln -s /henri/src/lib/picoweb/utils.py /henri/submodules/pycopy/ports/esp32/modules/picoweb/utils.py
-## ulogging
-ln -s /henri/src/lib/ulogging.py /henri/submodules/pycopy/ports/esp32/modules/ulogging.py
+if [ "${DEPS}" = "True" ]; then
+        ## pkg_resources
+        ln -s /henri/src/lib/pkg_resources.py /henri/submodules/pycopy/ports/esp32/modules/pkg_resources.py
+        ## uasyncio
+        mkdir /henri/submodules/pycopy/ports/esp32/modules/uasyncio
+        ln -s /henri/src/lib/uasyncio/__init__.py /henri/submodules/pycopy/ports/esp32/modules/uasyncio/__init__.py
+        ln -s /henri/src/lib/uasyncio/core.py /henri/submodules/pycopy/ports/esp32/modules/uasyncio/core.py
+        ## picoweb
+        mkdir /henri/submodules/pycopy/ports/esp32/modules/picoweb
+        ln -s /henri/src/lib/picoweb/__init__.py /henri/submodules/pycopy/ports/esp32/modules/picoweb/__init__.py
+        ln -s /henri/src/lib/picoweb/utils.py /henri/submodules/pycopy/ports/esp32/modules/picoweb/utils.py
+        ## ulogging
+        ln -s /henri/src/lib/ulogging.py /henri/submodules/pycopy/ports/esp32/modules/ulogging.py
+        fi
 ## henri
 if [ "${HENRI}" = "True" ]; then
         ln -s /henri/src/henri.py /henri/submodules/pycopy/ports/esp32/modules/henri.py
@@ -37,4 +39,11 @@ make -j4 -C mpy-cross
 make -j4 -C ports/esp32 ESPIDF=/opt/esp-idf
 
 # copy firmware out of the container
-cp ports/esp32/build/firmware.bin /build/firmware.bin
+if [ "${HENRI}" = "True" ] && [ "${DEPS}" = "True" ]; then
+        FILENAME="firmware-henri.bin"
+elif [ "${HENRI}" = "False" ] && [ "${DEPS}" = "True" ]; then
+        FILENAME="firmware-deps.bin"
+elif [ "${HENRI}" = "False" ] && [ "${DEPS}" = "False" ]; then
+        FILENAME="firmware.bin"
+fi
+cp ports/esp32/build/firmware.bin "/build/$FILENAME"
