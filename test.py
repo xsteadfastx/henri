660bbdd17fdd646a7aa89d31157cae60762d6020
Marvin Preuss <marvin@xsteadfastx.org>
2019-08-14 20:38:12 +0200

    run multiple tests and save exit codes

--- test.py
+++ test.py
@@ -4,8 +4,14 @@ import unittest
 sys.path.insert(0, "tests")
 sys.path.insert(0, "src")
 
-TESTS = ["test_foobar"]
+TESTS = ["test_foobar", "test_henri"]
+FAILURES_NUM = 0
 
 for test in TESTS:
-    m = __import__(test)
-    unittest.main(module=m)
+    try:
+        m = __import__(test)
+        unittest.main(module=m)
+    except BaseException:
+        FAILURES_NUM += 1
+
+sys.exit(FAILURES_NUM > 0)
